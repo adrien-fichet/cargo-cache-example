2d6482dcf6453e38e950ed8a7ffb96cdf984747e
Adrien FICHET <adrien.fichet@gmail.com>
2025-02-21 11:18:22 +0100

    Cache cowsay

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ use std::process::Command;
 use std::str;
 
 fn main() {
-    let cow = Command::new("cowsay").arg("hello").output().unwrap();
+    let cow = Command::new("cowsay")
+        .arg("hello")
+        .output()
+        .expect("Error running cowsay");
     let cow = str::from_utf8(&cow.stdout).unwrap();
     println!("{}", cow.bold().yellow().on_black());
 }
