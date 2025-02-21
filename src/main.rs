ea4ac458796eb3800aed418e1bd9904cc2397d7f
Adrien FICHET <adrien.fichet@gmail.com>
2025-02-21 11:00:22 +0100

    Cache only ~/.cargo

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use ansi_term::Color;
+use colored::Colorize;
 use std::process::Command;
 use std::str;
 
 fn main() {
     let cow = Command::new("cowsay").arg("hello").output().unwrap();
     let cow = str::from_utf8(&cow.stdout).unwrap();
-    println!("{}", Color::Yellow.bold().on(Color::Black).paint(cow));
+    println!("{}", cow.bold().yellow().on_black());
 }
